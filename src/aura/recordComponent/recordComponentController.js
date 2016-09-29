81c992d8922c055c37210e15618b890413d0c4fe
Ben Snyder <bsnyder@salesforce.com>
2016-09-29 15:46:25 -0400

    Missing files for data bound cmps

--- src/aura/recordComponent/recordComponentController.js
+++ src/aura/recordComponent/recordComponentController.js
@@ -1,5 +1,9 @@
 ({
 	handleInit : function(component, event, helper) {
 		helper.setRecord(component);
+	},
+    
+    handleRecordIdChange : function(component, event, helper) {
+		helper.setRecord(component);
 	}
 })
--- src/aura/recordComponent/recordComponentHelper.js
+++ src/aura/recordComponent/recordComponentHelper.js
@@ -2,21 +2,21 @@
 	setRecord : function(component) {
 		var action = component.get("c.getRecord");
 		var recordId = component.get("v.recordId");
-        
+
         if (!recordId) {
         	// Component's target ID has not yet been set so don't fire the server action.
         	return;
         }
         
         action.setParams({
-        	id : recordId
+        	recordId : recordId
         });
         
         action.setCallback(this, function(response){
             var state = response.getState();
-            
+
             if (state === "SUCCESS") {
-                cmp.set("v.targetRecord", response.getReturnValue());
+                component.set("v.targetRecord", response.getReturnValue());
             }
         });
         
